ff50f0fe529268885b25735b9783cb033b1d2e0a
Shufang Sun <fang_sunsf@yahoo.com>
2025-05-21 20:52:42 -0400

    basic component and settings more

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="dashboard">
+    <h2>My Tasks</h2>
+    <p>This is where your tasks will appear</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashboardView",
+};
+</script>
+
+<style scoped>
+.dashboard {
+  text-align: left;
+}
+</style>
